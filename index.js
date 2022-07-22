8cd6db289509bfa10d0f6a5baa455c71d1388f94
Andrew <48754372+hi5a@users.noreply.github.com>
2022-07-22 10:10:59 +0000

    changed the console.log output

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express"); 
+const express = require("express");
 const app = express();
 const fs = require("fs");
 const Discord = require("discord.js");
@@ -7,18 +7,20 @@ const prefix = "!";
 
 
 
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"],
- allowedMentions: ["users"] });
+const client = new Discord.Client({
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+  allowedMentions: ["users"]
+});
 client.commands = new Discord.Collection();
 const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"))
-for(file of commands) {
+for (file of commands) {
   const commandName = file.split(".")[0]
   const command = require(`./commands/${commandName}`)
   client.commands.set(commandName, command)
 }
 
 app.listen(3000, () => {
-  console.log("project is running!");
+  console.log("Discord bot is online!");
 })
 
 app.get("/", (req, res) => {
@@ -27,11 +29,11 @@ app.get("/", (req, res) => {
 
 
 client.on("messageCreate", message => {
-  if(message.content.startsWith(prefix)){
+  if (message.content.startsWith(prefix)) {
     const args = message.content.slice(prefix.length).trim().split(/ +/g)
     const commandName = args.shift()
     const command = client.commands.get(commandName)
-    if(!command) return 
+    if (!command) return
     command.run(client, message, args)
   }
 })
